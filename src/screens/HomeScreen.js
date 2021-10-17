83ca78491ca4c9c1e3ff45a6f927a2ca129285f8
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-10-17 17:52:37 +0700

    menambahkan Image sebagai card picture di home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {
+  Image,
   ScrollView,
   StyleSheet,
   Text,
@@ -9,10 +10,21 @@ import {
 
 import Icon from "react-native-vector-icons/FontAwesome";
 
+import hazelnutLatte from "../assets/image/products/hazelnutLatte.png";
+import cappuccino from "../assets/image/products/cappuccino.png";
+
 export default class HomeScreen extends Component {
   render() {
     return (
-      <View style={styles.parent}>
+      <ScrollView style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.mainTitle}>A good coffee is a good day</Text>
+          <Text style={styles.sectionTitle}>Favorite Products</Text>
+          <TouchableOpacity style={styles.linkWrapper}>
+            <Text style={styles.linkText}>See more</Text>
+          </TouchableOpacity>
+        </View>
+
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           {[...Array(20)].map((_i, idx) => (
             <TouchableOpacity
@@ -21,47 +33,114 @@ export default class HomeScreen extends Component {
               key={String(idx)}
             >
               <View style={styles.image}>
-                <Icon name="user-circle" size={30} color="#900" />
+                <Image source={hazelnutLatte} style={styles.img} />
               </View>
-              <Text style={styles.productName}>Hazelnut Latte</Text>
-              <Text style={styles.productPrice}>IDR 30.000</Text>
+
+              <View style={styles.textWrapper}>
+                <Text style={styles.productName}>Hazelnut Latte</Text>
+
+                <Text style={styles.productPrice}>IDR 30.000</Text>
+              </View>
             </TouchableOpacity>
           ))}
         </ScrollView>
-      </View>
+
+        <View style={styles.container}>
+          <Text style={styles.sectionTitle}>Promo for you</Text>
+          <TouchableOpacity style={styles.linkWrapper}>
+            <Text style={styles.linkText}>See more</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {[...Array(20)].map((_i, idx) => (
+            <TouchableOpacity
+              onPress={() => this.props.navigation.navigate("detail")}
+              style={styles.productCard}
+              key={String(idx)}
+            >
+              <View style={styles.image}>
+                <Image source={cappuccino} style={styles.img} />
+              </View>
+
+              <View style={styles.textWrapper}>
+                <Text style={styles.productName}>Cappuccino</Text>
+
+                <Text style={styles.productPrice}>IDR 24.000</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </ScrollView>
     );
   }
 }
+
+const ratio = 0.8;
 
 const styles = StyleSheet.create({
   parent: {
     backgroundColor: "#F2F2F2",
   },
+  container: {
+    paddingHorizontal: 34,
+  },
+  mainTitle: {
+    fontSize: 34,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  sectionTitle: {
+    color: "#6A4029",
+    fontWeight: "700",
+    fontSize: 17,
+  },
+  linkWrapper: {
+    width: 80,
+    alignSelf: "flex-end",
+  },
+  linkText: {
+    color: "#6A4029",
+    fontSize: 17,
+    textAlign: "right",
+  },
   productCard: {
     backgroundColor: "#fff",
-    height: 270,
-    width: 220,
+    height: 270 * ratio,
+    width: 220 * ratio,
     borderRadius: 30,
     elevation: 5,
     margin: 38,
     marginTop: 98,
     alignItems: "center",
   },
+  img: {
+    resizeMode: "contain",
+    width: "100%",
+  },
   image: {
-    width: 168,
-    height: 180,
+    width: 168 * ratio,
+    height: 189 * ratio,
     backgroundColor: "silver",
     borderRadius: 30,
     justifyContent: "center",
     alignItems: "center",
     marginTop: -60,
+    overflow: "hidden",
+  },
+  textWrapper: {
+    marginTop: 10,
+    justifyContent: "space-around",
+    height: 100,
   },
   productName: {
     fontWeight: "bold",
     fontSize: 22,
+    textAlign: "center",
   },
   productPrice: {
     fontWeight: "bold",
     color: "#6A4029",
+    textAlign: "center",
   },
 });
